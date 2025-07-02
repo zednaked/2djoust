8057eead18f1189d79388012a35c858f81883393
zednaked <zednaked@gmail.com>
2025-07-02 20:34:07 -0300

    feature: powerup de vida

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751499044|7590617065';
+const CACHE_VERSION = '1751499238|7785274524';
 /** @type {string} */
 const CACHE_PREFIX = '2djoust-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
