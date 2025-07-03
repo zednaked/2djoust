5dbbc9501f5d3a5cafac5e616f11cf80c829c340
zednaked <zednaked@gmail.com>
2025-07-03 11:13:43 -0300

    chore: melhors particulas

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751551196|59743124239';
+const CACHE_VERSION = '1751552014|60561251972';
 /** @type {string} */
 const CACHE_PREFIX = '2djoust-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
