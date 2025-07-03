244068da817306e89659a916f59d0b9d24265f60
zednaked <zednaked@gmail.com>
2025-07-03 10:47:40 -0300

    feature:  powerups

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751505495|14041888238';
+const CACHE_VERSION = '1751550452|58998562786';
 /** @type {string} */
 const CACHE_PREFIX = '2djoust-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
